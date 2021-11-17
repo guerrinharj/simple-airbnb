45cfd798ae420445f7ab040c345a53561c664351
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2021-11-17 15:17:06 -0300

    added show and new / create controllers

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,9 +1,33 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show]
+
   def index
     @flats = Flat.all
   end
 
+  def show
+  end
+
   def new
     @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(restaurant_params)
+    if @flat.save
+      redirect_to flat_path(@flat[:id])
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
   end
 end
